badd98e75ce621f03a2065f9f4a83ca562d46d18
unknown <mdtanvir7462@gmail.com>
2023-06-10 01:52:01 +0600

    data added to mychart

--- src/Components/PopulerClass/PopularCard.jsx
+++ src/Components/PopulerClass/PopularCard.jsx
@@ -1,56 +1,38 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../Provider/AuthProvider';
-import { useNavigate } from 'react-router-dom';
-
-const PopularCard = ({ Singleclass }) => {
-  const {user} = useContext(AuthContext) 
-  const navigate = useNavigate()
-  const { className, students, image, price, instructor,totalSeats , Availableseats} = Singleclass;
-
-
-
-const handleSelect = (classes)=>{
-  const {Availableseats,className,  email, image,instructor,price,students,_id} = classes
-  
-  if(user){
-    console.log(user)
-const myCartdata =  {}
-  }
-  else{
-    navigate('/login')
-    }
-
-
-}
+const PopularCard = ({ Singleclass ,handleSelect}) => {
+  const { className, students, image, price, instructor,  Availableseats } = Singleclass;
 
 
   return (
     <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-700 hover:scale-95">
-      <img src={image} alt="class" className="  w-full h-48 object-cover transition duration-700 ease-in-out transform hover:scale-110 " />
-      <div className=" p-4">
-       <div className=''>
-     
-       <h2 className="text-2xl font-bold mb-2 text-yellow-500" >{className}</h2>
-        <p className="text-gray-600 mb-2">
-          <strong>Instructor:</strong> {instructor}
-        </p>
-        
-        <p className="text-gray-600 mb-2">
-          <strong>Price:</strong> ${price}
-        </p>
-      
-       
-       <p className="text-gray-600 mb-2">
-          <strong>Abailable-Seats :</strong> {Availableseats}
-        </p>
-        <p className="text-gray-600 mb-2">
-          <strong>Students :</strong> {students}
-        </p>
-     
-       </div>
+      <img
+        src={image}
+        alt="class"
+        className="w-full h-48 object-cover transition duration-700 ease-in-out transform hover:scale-110"
+      />
+      <div className="p-4">
+        <div>
+          <h2 className="text-2xl font-bold mb-2 text-yellow-500">{className}</h2>
+          <p className="text-gray-600 mb-2">
+            <strong>Instructor:</strong> {instructor}
+          </p>
+          <p className="text-gray-600 mb-2">
+            <strong>Price:</strong> ${price}
+          </p>
+          <p className="text-gray-600 mb-2">
+            <strong>Available Seats:</strong> {Availableseats}
+          </p>
+          <p className="text-gray-600 mb-2">
+            <strong>Students:</strong> {students}
+          </p>
+        </div>
         <div className="flex justify-center">
-          
-          <button className='btn btn-warning btn-outline' onClick={()=>handleSelect(Singleclass)}>Book Mark</button>
+          <button
+            className="btn btn-warning btn-outline"
+            onClick={() => handleSelect(Singleclass)}
+           
+          >
+            Book Mark
+          </button>
         </div>
       </div>
     </div>
@@ -58,3 +40,5 @@ const myCartdata =  {}
 };
 
 export default PopularCard;
+
+
--- src/Components/PopulerClass/PopulerClass.jsx
+++ src/Components/PopulerClass/PopulerClass.jsx
@@ -1,127 +1,88 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PopularCard from './PopularCard';
+import { AuthContext } from '../Provider/AuthProvider';
+import { useNavigate } from 'react-router-dom';
 
 
 const PopulerClass = () => {
+  const { User } = useContext(AuthContext);
+  const navigate = useNavigate();
   const [classes, setClass] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:5000/instractorclass')
       .then(res => res.json())
       .then(data => {
-        if(data.length >6){
-          setClass(data.slice(0,6))
+        if (data.length > 6) {
+          setClass(data.slice(0, 6))
         }
-        else{
-                  setClass(data)
-                }
-        
+        else {
+          setClass(data)
+        }
+
       })
   }, [])
 
 
+  const handleSelect = (classes) => {
+    const { Availableseats, className, email, image, instructor, price, students, _id } = classes;
 
-  // [
-  //   {
-  //     "classId": 1,
-  //     "className": "Hip Hop Dance",
-  //     "instractor": "MD TAJUL ISLAM TANVIR",
-  //     "seats" : 50,
-  //     "students": 10,
-  //     "image": "https://i.ibb.co/R3JQHhZ/240-F-449068256-e-Nw-BShm-GCE799k-KCy-Kl5agb-Vc-Nwuk72-G.jpg",
-  //     "email": "mdtanvir7462@gmail.com",
-  //     "price": 200
-  //   },
-  //   {
-  //     "classId": 2,
-  //     "className": "Ballet",
-  //     "instractor": "MD Evan",
-  //     "seats" : 50,
-  //     "students": 15,
-  //     "image": "https://i.ibb.co/80G2CVN/web-ae-gracedance-courtesy.jpg",
-  //     "email": "mdtanvir7463@gmail.com",
-  //     "price": 180
-  //   },
-  //   {
-  //     "classId": 3,
-  //     "className": "Contemporary Dance",
-  //     "instractor": "MD TANVIR",
-  //     "seats" : 50,
-  //     "students": 17,
-  //     "image": "https://i.ibb.co/n8Gs4RT/240-F-538866550-55-G0l7-Q8y-Hfp-Q5-NIQ7-K95-ZVt-FYc-Mvgg-C.jpg",
-  //     "email": "mami@gmail.com",
-  //     "price": 220
-  //   },
-  //   {
-  //     "classId": 4,
-  //     "className": "Salsa Dance",
-  //     "instractor": "Md Tanvir",
-  //     "seats" : 50,
-  //     "students": 9,
-  //     "image": "https://i.ibb.co/5T8mC7Z/240-F-277242783-Ub83l-Wi91s2-Vnm-RIhmk-D6q3n-Z3ogtb-JE.jpg",
-  //     "email": "new@gmail.com",
-  //     "price": 230
-  //   },
-  //   {
-  //     "classId": 5,
-  //     "className": "Jazz Dance",
-  //     "instractor": "Md Tanvir",
-  //     "seats" : 50,
-  //     "students": 7,
-  //     "image": "https://i.ibb.co/f1FKb9X/asian-dancers-standing-one-foot-1098-22135.jpg",
-  //     "email": "new@gmail.com",
-  //     "price": 190
-  //   },
-  //   {
-  //     "classId": 6,
-  //     "className": "belly Dance",
-  //     "instractor": "Md Tanvir",
-  //     "seats" : 50,
-  //     "students": 11,
-  //     "image": "https://i.ibb.co/g9BpmWZ/sm-ballet-crop.jpg",
-  //     "email": "new@gmail.com",
-  //     "price": 250
-  //   },
-  //   {
-  //     "classId": 7,
-  //     "className": "Tap Dance",
-  //     "instractor": "MD TAJUL ISLAM TANVIR",
-  //     "seats" : 50,
-  //     "students": 6,
-  //     "image": "https://i.ibb.co/kyRzQy8/240-F-257036699-P75-JW1i77y6uk-Iof-LXs-Q6-Zsg-F7-Hh2-Lg-I.jpg",
-  //     "email": "new@gmail.com",
-  //     "price": 88
-  //   },
-  //   {
-  //     "classId": 8,
-  //     "className": "stylish Dance",
-  //     "instractor": "	Md salib",
-  //     "seats" : 50,
-  //     "students": 14,
-  //     "image": "https://i.ibb.co/g9BpmWZ/sm-ballet-crop.jpg",
-  //     "email": "mdtareqaziz11234@gmail.com",
-  //     "price": 231
-  //   }
-  // ]
+    if (User) {
+      const myCartdata = {
+        Availableseats,
+        className,
+        instructorEmail: email,
+        insName: instructor,
+        oldId: _id,
+        price,
+        students,
+        classImage: image,
+        userName: User.displayName,
+        userEmail: User.email,
+        userPhoto: User.photoURL
+      };
+
+      fetch('http://localhost:5000/mybookmark', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(myCartdata)
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.statusbar === 'Already Bookmarked') {
+          console.log('sfhjksd')
+         
+          
+          }
+        });
+    } else {
+      navigate('/login');
+    }
+  };
+
+
+
 
 
   const sortedClasses = classes.sort((a, b) => b.students - a.students);
-  
+
 
 
   return (
-  
 
-<section className="bg-gray-100 pt-32 pb-10">
-<div className="container mx-auto w-11/12 ">
-  <h2 className="text-3xl font-bold text-center mb-16 ">Popular Classes</h2>
-  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10" >
-{
-  sortedClasses.map(Singleclass => <PopularCard key={Singleclass._id} Singleclass={Singleclass}></PopularCard>)
-}
-    </div>
-</div>
-</section>
+
+    <section className="bg-gray-100 pt-32 pb-10">
+      <div className="container mx-auto w-11/12 ">
+        <h2 className="text-3xl font-bold text-center mb-16 ">Popular Classes</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10" >
+          {
+            sortedClasses.map(Singleclass => <PopularCard key={Singleclass._id} Singleclass={Singleclass} handleSelect={handleSelect}></PopularCard>)
+          }
+        </div>
+      </div>
+    </section>
 
   );
 };
